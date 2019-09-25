cc230a1ef99b06caf83c08816cb693d316451daf
EightyEighth <denisrozlomiy@gmail.com>
2019-09-25 17:13:07 +0300

    added compiled lib

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
+import { BlockCypherProvider } from "./bitcoin";
+import { InfuraProvider} from "./ethereum";
 
-
+module .exports = {
+    BlockCypherProvider: BlockCypherProvider,
+    InfuraProvider: InfuraProvider,
+};
 
